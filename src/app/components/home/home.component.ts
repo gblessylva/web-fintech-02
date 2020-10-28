ccfbb14fee9580de381cb8f9294ba4078d0f6528
gblessylva <gblessylva@gmail.com>
2020-10-28 06:05:30 +0100

    FEAT: added beneficiary component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import {NgModule, Component, OnInit } from '@angular/core';
 import {HeroComponent} from '../hero/hero.component';
-import {FooterComponent} from '../footer/footer.component'
+import {FooterComponent} from '../footer/footer.component';
+import {HowWorksSectionComponent} from '../how-works-section/how-works-section.component'
+import {BlogSectionComponent} from '../blog-section/blog-section.component';
+import {FunFactsComponent} from '../fun-facts/fun-facts.component';
+import {BeneficiariesComponent  } from "../beneficiaries/beneficiaries.component";
 
 @NgModule({
   declarations:[
     HeroComponent, 
-    FooterComponent
+    FooterComponent,
+    HowWorksSectionComponent,
+    BlogSectionComponent,
+    FunFactsComponent,
+    BeneficiariesComponent
   ]
 })
 
