15d9a3ad9752db7c9fb80787bcc872166f5e942a
gblessylva <gblessylva@gmail.com>
2020-11-02 14:09:53 +0100

    pacth: Nav bar modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -59,7 +59,8 @@ import { FaqComponent } from './components/faq/faq.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, 
+    BrowserAnimationsModule
   ],
   providers: [AuthService, AuthGuardService, MessageService],
   bootstrap: [AppComponent]
